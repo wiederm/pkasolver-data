33e9b17003f9a3bc25792bed44b27f35bb24f987
Fritz Mayr <fmayr@a7srv2.pch.univie.ac.at>
2022-01-18 01:15:03 +0100

    small changes to docomentation and path errors in scripts fixed

--- scripts/training_model.sh
+++ scripts/training_model.sh
@@ -3,10 +3,11 @@
 # ChEMBL dataset and then fine tuning on the experimental dataset
 
 # path to scirpts
-data_path='../'
 run=0
+dir_path=$(dirname "$0")
+data_path=${dir_path}/..
 
 # start with pretraining on the CHEMBL data
-python 06_training.py --input ${data_path}/05_chembl_dataset_pyg.pkl --path ${data_path}/trained_models/training_run_${run} --epoch 1000
+python ${dir_path}/06_training.py --input ${data_path}/05_chembl_dataset_pyg.pkl --path ${data_path}/trained_models/training_run_${run} --epoch 1000
 # transfer learning on the experimental data
-python 06_training.py --input ${data_path}/05_experimental_training_datasets_pyg.pkl --path ${data_path}/trained_models/training_run_${run} -r --epoch 1000 --reg ${data_path}/05_chembl_dataset_pyg.pkl
+python ${dir_path}/06_training.py --input ${data_path}/05_experimental_training_datasets_pyg.pkl --path ${data_path}/trained_models/training_run_${run} -r --epoch 1000 --reg ${data_path}/05_chembl_dataset_pyg.pkl
